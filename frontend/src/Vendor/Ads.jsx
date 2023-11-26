8e1734436b03f3ef510e3026dbf5f56412b32248
Ramya M <ramyamv708@gmail.com>
2023-11-26 10:31:42 +0530

    addeatils ui

--- frontend/src/Vendor/Ads.jsx
+++ frontend/src/Vendor/Ads.jsx
@@ -1,53 +1,56 @@
-import React from 'react'
-import Card from './components/Card'
-import { useEffect, useState } from 'react'
-import { HashLoader } from 'react-spinners'
-import PerformRequest from '../api/axios'
+import React from "react";
+import Card from "./components/Card";
+import { useEffect, useState } from "react";
+import { HashLoader } from "react-spinners";
+import PerformRequest from "../api/axios";
 const Ads = () => {
-    const [loading, setLoading] = useState(true)
-    const [ads, setAds] = useState([])
-    useEffect(() => {
-        PerformRequest('/api/ad/get/all', 'GET')
-            .then((data) => {
-                console.log(data);
-                setLoading(false)
-                setAds(data)
-            })
-    }, [])
+  const [loading, setLoading] = useState(true);
+  const [ads, setAds] = useState([]);
+  useEffect(() => {
+    PerformRequest("/api/ad/get/all", "GET").then((data) => {
+      console.log(data);
+      setLoading(false);
+      setAds(data);
+    });
+  }, []);
 
-    return (
-        <div>
-            <div>
-                {
-                    !loading ?
-                        <div>
-                            <h1 className='m-6 text-2xl font-extralight'>Find customers</h1>
-                            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 '>
-                                {ads.map((Element, key) => {
-                                    return <div className='flex justify-center '>
-                                        <Card key={key}
-                                            desc={Element.desc}
-                                            images={Element.images}
-                                            category={Element.services.category}
-                                            expectedPrice={Element.services.expectedPrice.$numberDecimal} />
-                                    </div>
-                                })}
-                            </div>
-                        </div>
-                        :
-                        <HashLoader
-                            className='absolute mt-[50%] md:mt-[150px] mx-auto align-middle '
-                            color='#36d7b7'
-                            loading={loading}
-                            size={150}
-                            aria-label="Loading Spinner"
-                            data-testid="loader"
-                        />
-                }
-
+  return (
+    <div>
+      <div>
+        {!loading ? (
+          <div>
+            <h1 className="m-6 text-2xl font-extralight">Find customers</h1>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+              {ads.map((Element, key) => {
+                return (
+                  <div className="flex justify-center ">
+                    <Card
+                      key={key}
+                      desc={Element.desc}
+                      images={Element.images}
+                      category={Element.services.category}
+                      expectedPrice={
+                        Element.services.expectedPrice.$numberDecimal
+                      }
+                    />
+                  </div>
+                );
+              })}
             </div>
-        </div>
-    )
-}
+          </div>
+        ) : (
+          <HashLoader
+            className="absolute mt-[50%] md:mt-[150px] mx-auto align-middle "
+            color="#36d7b7"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Ads
+export default Ads;
