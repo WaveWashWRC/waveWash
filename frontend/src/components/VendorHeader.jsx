0d1144c82915f01369c6a0b9288b0f50592b8b93
Ramya M <ramyamv708@gmail.com>
2023-11-28 18:18:40 +0530

    responsive and hamburger menu

--- frontend/src/components/VendorHeader.jsx
+++ frontend/src/components/VendorHeader.jsx
@@ -1,10 +1,5 @@
 import React, { Fragment } from "react";
-import { Menu, Popover, Transition } from "@headlessui/react";
-import {
-  HiOutlineBell,
-  HiOutlineSearch,
-  HiOutlineChatAlt,
-} from "react-icons/hi";
+import { Menu, Transition } from "@headlessui/react";
 import { useNavigate } from "react-router-dom";
 import classNames from "classnames";
 import { CgProfile } from "react-icons/cg";
@@ -14,76 +9,10 @@ export const VendorHeader = () => {
 
   return (
     <div className="bg-white h-16 px-4 flex items-center border-b border-gray-200 justify-between">
-      
-      <div className="flex items-center gap-2 mr-2">
-        <Popover className="relative">
-          {({ open }) => (
-            <>
-              <Popover.Button
-                className={classNames(
-                  open && "bg-gray-100",
-                  "group inline-flex items-center rounded-sm p-1.5 text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100"
-                )}
-              >
-                <HiOutlineChatAlt fontSize={24} />
-              </Popover.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 translate-y-1"
-                enterTo="opacity-100 translate-y-0"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 translate-y-1"
-              >
-                <Popover.Panel className="absolute right-0 z-10 mt-2.5 transform w-80">
-                  <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
-                    <strong className="text-gray-700 font-medium">
-                      Messages
-                    </strong>
-                    <div className="mt-2 py-1 text-sm">
-                      This is messages panel.
-                    </div>
-                  </div>
-                </Popover.Panel>
-              </Transition>
-            </>
-          )}
-        </Popover>
-        <Popover className="relative">
-          {({ open }) => (
-            <>
-              <Popover.Button
-                className={classNames(
-                  open && "bg-gray-100",
-                  "group inline-flex items-center rounded-sm p-1.5 text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100"
-                )}
-              >
-                <HiOutlineBell fontSize={24} />
-              </Popover.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 translate-y-1"
-                enterTo="opacity-100 translate-y-0"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 translate-y-1"
-              >
-                <Popover.Panel className="absolute right-0 z-10 mt-2.5 transform w-80">
-                  <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
-                    <strong className="text-gray-700 font-medium">
-                      Notifications
-                    </strong>
-                    <div className="mt-2 py-1 text-sm">
-                      This is notification panel.
-                    </div>
-                  </div>
-                </Popover.Panel>
-              </Transition>
-            </>
-          )}
-        </Popover>
+      <div className="flex items-center gap-2"></div>
+
+      <div className="flex items-center">
+        {/* Profile Menu */}
         <Menu as="div" className="relative">
           <div>
             <Menu.Button className="ml-2 bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-neutral-400">
@@ -149,3 +78,5 @@ export const VendorHeader = () => {
     </div>
   );
 };
+
+export default VendorHeader;
