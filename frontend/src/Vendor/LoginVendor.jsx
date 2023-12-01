2b5c89d2c96d1b83d4e3659c412b8b75c5ec69ad
Ramya M <ramyamv708@gmail.com>
2023-12-01 19:32:55 +0530

    ui changes

--- frontend/src/Vendor/LoginVendor.jsx
+++ frontend/src/Vendor/LoginVendor.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import loginImg from "/images/login.jpg";
+import loginImg from "../assets/loginimg.jpg";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import service from "../assets/service.jpg";
@@ -42,66 +42,72 @@ const LoginVendor = () => {
     }
   };
   return (
-    <div className="bg-gradient-to-br flex-wrap  from-cyan-400 to-base-400  h-screen relative flex items-center justify-center">
-      <div className="flex justify-center  ">
-        <div>
-          <h1 className="font-extrabold text-white  text-6xl p-4">
-            WaveWash for Vendors
-          </h1>
-          {/* <p className="font-bold m-2 text-2xl">Get Started in few steps</p> */}
-          <img
-            className="shadow-lg sm:block hidden rounded-lg m-2 w-[600px]"
-            src={service}
-            alt="loading.."
-          />
-        </div>
-      </div>
+    <div className="bg-gradient-to-br flex-wrap  from-cyan-600 to-base-400  h-screen relative flex items-center justify-center">
       <div
-        className=" h-[600px] flex justify-center items-center  mx-auto overflow-hidden rounded-lg "
+        className="grid grid-cols-1 sm:grid-cols-2 h-[500px] sm:w-[80%] md:w-[60%] mx-auto overflow-hidden rounded-lg shadow-2xl"
         id="login-card"
       >
-        <div className="w-full">
+        <div className="relative">
+          <img
+            src={loginImg}
+            alt="Login Wash Image"
+            className="w-full h-full object-cover"
+          />
+          <div className="flex flex-col justify-center items-center text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <h1 className="text-xl md:text-5xl text-base-400 font-bold sm:mb-0 md:mb-4">
+              WavesWash for Vendors
+            </h1>
+            <h1 className="text-base md:text-2xl font-semibold">
+              Log in to your account.
+            </h1>
+          </div>
+        </div>
+        <div className="bg-base-400 m-2 md:m-0 p-2 md:py-8 flex items-center flex-wrap justify-center">
           <form
-            action=""
+            className="max-w-[700px] w-full mx-auto bg-base-400 md:py-8 px-4 py-2 md:px-16 rounded-lg"
             onSubmit={handleRegister}
-            className="border-2   border-blue-400 w-full  mx-auto bg-base-400 p-8 px-16 rounded-lg"
           >
-            <h2 className="text-2xl text-gray-200 font-bold text-left">
-              Log into your account
+            <h2 className="text-lg md:text-2xl dark:text-gray-200 font-bold text-left mb-1 md:mb-4">
+              LOGIN
             </h2>
-
-            <div className="flex flex-row items-center justify-between space-x-2  pt-2 text-lg">
-              <label htmlFor="email" className="text-gray-200">
+            <div className="flex flex-col mb-2 md:mb-4">
+              <label
+                htmlFor="emailid"
+                className="text-sm md:text-base text-gray-200 mb-1 md:mb-2"
+              >
                 Email ID
               </label>
               <input
                 type="email"
                 id="email"
-                placeholder="smoothgears@gmail.com"
+                placeholder="jsmoothgears@gmail.com"
                 onChange={(e) => setEmailID(e.target.value)}
                 value={emailID}
-                className="rounded-sm border text-base w-[13.8rem] bg-gray-300 mt-2 px-2 py-3 h-8 focus:text-gray-300 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                className="rounded-sm p-2 text-xs md:text-base bg-gray-300 md:py-2 md:px-3 focus:text-gray-300 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
               />
             </div>
-
-            <div className="flex flex-row items-center justify-between space-x-2 pt-2 text-lg">
-              <label htmlFor="password" className="text-gray-200">
+            <div className="flex flex-col mb-2 md:mb-4">
+              <label
+                htmlFor="password"
+                className="text-sm md:text-base text-gray-200 mb-1 md:mb-2"
+              >
                 Password
               </label>
               <input
                 type="password"
                 id="password"
+                placeholder="*********"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
-                className="rounded-sm w-[13.8rem] border bg-gray-300 mt-2 px-2 py-3 h-8 focus:text-gray-300 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                className="rounded-sm p-2 text-xs md:text-base bg-gray-300 md:py-2 md:px-3 focus:text-gray-300 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
               />
             </div>
 
-            {/* <div className="flex justify-between text-gray-200 my-3">
+            {/* <div className="flex justify-between text-gray-200 my-3 text-[0.5rem] md:text-sm">
               <p className="flex items-center">
                 <input
                   type="checkbox"
-                  className="form-checkbox mr-2 text-blue-500 h-5 w-5"
+                  className="form-checkbox md:mr-1 text-blue-500 w-4 md:w-5"
                 />
                 Remember Me
               </p>
@@ -109,23 +115,25 @@ const LoginVendor = () => {
                 Forgot Password?
               </a>
             </div> */}
-            <div className="flex">
-              <button className="w-44 border border-white hover:bg-cyan-800 mx-auto py-2 my-4  text-gray-300 text-xl bg-base-600 rounded-lg shadow-lg hover:bg-base-600/80">
-                LOGIN
+            <div className="flex justify-center">
+              <button
+                className="md:w-44 px-4 py-2 md:py-2 text-gray-300 text-sm md:text-xl bg-blue-500 rounded-lg shadow-lg hover:bg-base-600/80"
+                type="submit"
+              >
+                SIGN IN
               </button>
             </div>
-            <div className="flex justify-center text-gray-300 text-sm space-x-1">
-              <p>New to us ?</p>
-              <Link
-                to="/register"
-                className="hover:underline hover:cursor-pointer"
-              >
-                REGISTER
-              </Link>
-            </div>
           </form>
+          <div className="flex justify-center text-gray-300 text-[0.6rem] md:text-sm md:mt-4">
+            <p>Don't have an account?</p>
+            <Link
+              to="/register"
+              className="hover:underline hover:cursor-pointer ml-1"
+            >
+              SIGN UP!
+            </Link>
+          </div>
         </div>
-        <ToastContainer />
       </div>
     </div>
   );
