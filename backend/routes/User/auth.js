3370d3310ea17282e3cfe88ab5ce842b3f911e9a
Ramya M <ramyamv708@gmail.com>
2023-12-31 21:51:41 +0530

    services array

--- backend/routes/User/auth.js
+++ backend/routes/User/auth.js
@@ -6,9 +6,13 @@ const {
   loginUser,
   getUser,
 } = require("../../controllers/User/userAuth");
+
+const { getVendorsByService } = require("../../controllers/User/vendorInfo");
 const authenticate = require("../../middleware/authenticate");
 
 router.get("/user", authenticate, getUser); //responds with json only when Status is OK
+
+router.get("/vendors/by-service/:service", getVendorsByService); // to fetch vendors by service
 
 router.post("/register", registerUser); //post with body --> email & password
 
